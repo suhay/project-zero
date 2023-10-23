ed97b7aa2d1066867247a0310bd00e5103f8032e
Matt Suhay <matthew.suhay@gmail.com>
2023-10-23 18:27:35 -0500

    Cleaning up some extra packages

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import React from "react";
 import { account } from "../utils/appwrite";
 import { ID, Models } from "appwrite";
 import { verifyEmail, googleAuth, login } from "../utils/auth";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function SignUp() {
@@ -44,31 +43,31 @@ export default function SignUp() {
           <h2>Sign up</h2>
           <form onSubmit={(e) => handleSubmit(e)}>
             <input
-              id='username'
-              type='text' //
-              placeholder='username'
+              id="username"
+              type="text" //
+              placeholder="username"
               value={user.username}
-              data-testid='username'
+              data-testid="username"
               required
               onChange={(e) => setUser({ ...user, username: e.target.value })}
             />
             <hr />
             <input
-              id='email'
-              type='email' //
-              placeholder='Email address'
+              id="email"
+              type="email" //
+              placeholder="Email address"
               value={user.email}
-              data-testid='email'
+              data-testid="email"
               required
               onChange={(e) => setUser({ ...user, email: e.target.value })}
             />
             <hr />
             <input
-              id='password'
-              type='password' //
-              placeholder='password'
+              id="password"
+              type="password" //
+              placeholder="password"
               value={user.password}
-              data-testid='password'
+              data-testid="password"
               required
               onChange={(e) => setUser({ ...user, password: e.target.value })}
             />
@@ -76,7 +75,7 @@ export default function SignUp() {
             <button> Verify Email </button>
             <br />
             <h2>or</h2>
-            <button type='button' onClick={(e) => signWithGoogle(e)}>
+            <button type="button" onClick={(e) => signWithGoogle(e)}>
               {" "}
               Continue with Google{" "}
             </button>
