4d33abaf232f1da113244b0b94de564f86a48270
Matt Suhay <matthew.suhay@gmail.com>
2023-10-24 19:50:31 -0500

    Adding in article placeholder design

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&family=Source+Serif+4:opsz,wght@8..60,300&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/types/posts.ts
+++ src/types/posts.ts
@@ -8,6 +8,7 @@ export type Post = {
   };
   date: DateTime;
   content?: string;
+  coverImage?: string;
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   [key: string]: any;
 };
