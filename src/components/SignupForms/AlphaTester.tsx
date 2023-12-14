074db50e17e7b2c437ffc0da524f841c1e6208c4
Matt Suhay <matthew.suhay@gmail.com>
2023-12-14 12:20:41 -0600

    Adding recaptcha (#30)
    * Adding recaptcha
    
    * Moving context out of layout

--- src/components/SignupForms/AlphaTester.tsx
+++ src/components/SignupForms/AlphaTester.tsx
@@ -1,21 +1,25 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import Image from "next/image";
+import {
+  GoogleReCaptchaProvider,
+  useGoogleReCaptcha,
+} from "react-google-recaptcha-v3";
 import { SubmitHandler, useForm } from "react-hook-form";
 
-import { Input } from "~/Form/Input";
-import { Button } from "~/Button";
 import { executeFunction } from "@/src/utils/functions";
+import { Button } from "~/Button";
+import { Input } from "~/Form/Input";
 import { Layout } from "../lib/Layout";
 
 type Input = {
   email: string;
-  b_2de017cc5896a770dd4853b99_6c0ae287ae: string;
 };
 
 export function AlphaTesterForm() {
   const { register, handleSubmit } = useForm<Input>();
+  const { executeRecaptcha } = useGoogleReCaptcha();
 
   const [alreadySignedUp, setAlreadySignedUp] = useState(true);
   const [justSignup, setJustSignedUp] = useState(false);
@@ -28,10 +32,22 @@ export function AlphaTesterForm() {
     }
   }, []);
 
+  const handleReCaptchaVerify = useCallback(async () => {
+    if (!executeRecaptcha) {
+      return;
+    }
+
+    return await executeRecaptcha("alpha-submit");
+  }, [executeRecaptcha]);
+
   const onSubmit: SubmitHandler<Input> = async (data) => {
     setIsLoading(true);
+    const token = await handleReCaptchaVerify();
     executeFunction(process.env.NEXT_PUBLIC_APPWRITE_MAILCHIMP_FUNCTION ?? "", {
-      message: data,
+      message: {
+        ...data,
+        token,
+      },
     })
       .then(() => {
         setJustSignedUp(true);
@@ -50,58 +66,56 @@ export function AlphaTesterForm() {
   }
 
   return (
-    <div className="container mx-auto">
-      <Layout.Grid className="items-center snap-start h-[512px] my-8">
-        <section>
-          {!justSignup && (
-            <>
-              <h2>Become an Alpha tester!</h2>
-              <form
-                className="mb-0 mt-6 space-y-4 p-4 sm:p-6 lg:p-8"
-                onSubmit={handleSubmit(onSubmit)}
-              >
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_GOOGLE_CAPTCHA_V3 ?? ""}
+    >
+      <div className="container mx-auto">
+        <Layout.Grid className="items-center snap-start h-[512px] my-8">
+          <section>
+            {!justSignup && (
+              <>
+                <h2>Become an Alpha tester!</h2>
+                <form
+                  className="mb-0 mt-6 space-y-4 p-4 sm:p-6 lg:p-8"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  <p className="text-center text-lg font-medium">
+                    Let's break some things and earn points doing it!
+                  </p>
+                  <Input
+                    label="Email"
+                    type="email"
+                    placeholder="Email"
+                    autoComplete="off"
+                    {...register("email", { required: "Email is required" })}
+                  />
+                  <Button.Simple
+                    type="submit"
+                    label="Sign up"
+                    isLoading={isLoading}
+                  />
+                </form>
+              </>
+            )}
+            {justSignup && (
+              <div className="text-center">
                 <p className="text-center text-lg font-medium">
-                  Let's break some things and earn points doing it!
+                  Thanks for signing up! We’ll be in touch soon.
                 </p>
-                <Input
-                  label="Email"
-                  type="email"
-                  placeholder="Email"
-                  autoComplete="off"
-                  {...register("email", { required: "Email is required" })}
-                />
-                <input
-                  type="text"
-                  tabIndex={-1}
-                  value=""
-                  {...register("b_2de017cc5896a770dd4853b99_6c0ae287ae")}
-                />
-                <Button.Simple
-                  type="submit"
-                  label="Sign up"
-                  isLoading={isLoading}
-                />
-              </form>
-            </>
-          )}
-          {justSignup && (
-            <div className="text-center">
-              <p className="text-center text-lg font-medium">
-                Thanks for signing up! We’ll be in touch soon.
-              </p>
-            </div>
-          )}
-        </section>
-        <section className="flex justify-center">
-          <Image
-            src="/assets/local-scoop.jpg"
-            alt={""}
-            width={512}
-            height={512}
-          />
-        </section>
-      </Layout.Grid>
-      <div className="flex border border-gray-600" />
-    </div>
+              </div>
+            )}
+          </section>
+          <section className="flex justify-center">
+            <Image
+              src="/assets/local-scoop.jpg"
+              alt={""}
+              width={512}
+              height={512}
+            />
+          </section>
+        </Layout.Grid>
+        <div className="flex border border-gray-600" />
+      </div>
+    </GoogleReCaptchaProvider>
   );
 }
